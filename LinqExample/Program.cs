1448e66c682158dfa58131178ba9cc0f89113650
user <user@LPC2AUTOITPROG>
2020-10-22 14:00:05 +0600

    first

--- LinqExample/Program.cs
+++ LinqExample/Program.cs
@@ -10,6 +10,18 @@ namespace LinqExample
     {
         static void Main(string[] args)
         {
+            string[] teams = { "Бавария", "Боруссия", "Реал Мадрид", "Манчестер Сити", "ПСЖ", "Барселона" };
+
+            var selectTeams = from t in teams
+                              where t.ToUpper().StartsWith("Б")
+                              orderby t
+                              select t;
+            foreach  (string s in selectTeams)
+            {
+                Console.WriteLine(s);
+            }
+
+            Console.ReadKey();
         }
     }
 }
